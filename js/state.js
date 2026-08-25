eb191a9f4eac5c6715792448ffdcd8808409c102
linzhongyoumeng <fwddny520@gmail.com>
2026-08-25 10:51:21 +0800

    v2.1.3: Fix batch CPU mode with -nogpu, fix pro mode all params, 22/22 tests passed

--- js/state.js
+++ js/state.js
@@ -3,5 +3,5 @@ let isProcessing = false;
 let processStartTime = 0;
 let progressUnsubscribe = null; // 进度监听器取消函数
 
-const APP_VERSION = '2.1.2';
+const APP_VERSION = '2.1.3';
 const GITHUB_REPO = 'linzhongyoumeng/dds-texture-tool';
